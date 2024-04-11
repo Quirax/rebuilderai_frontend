b4b6f38f5ec8c0f141f6942a30017dc46b6dd57a
Quirax Lee <quiraxical@gmail.com>
2024-04-11 17:22:45 +0900

    Applied pretendard font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './Locale'
+import 'pretendard/dist/web/variable/pretendardvariable.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
